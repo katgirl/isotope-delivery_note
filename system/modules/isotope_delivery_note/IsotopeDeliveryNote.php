766b99537602bd7d9ac04d07449cae708d1b5a00
Kirsten Roschanski <github@kat-webdesign.de>
2012-06-22 18:07:47 +0200

    first commit

---
+++ system/modules/isotope_delivery_note/IsotopeDeliveryNote.php
@@ -0,0 +1,150 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2011 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Kirsten Roschanski &#40;2012&#41; 
+ * @author     Kirsten Roschanski 
+ * @package    Isotope
+ * @license    LGPL 
+ * @filesource
+ */
+ 
+class IsotopeDeliveryNote extends IsotopeProductCollection
+{
+
+	/**
+	 * Name of the current table
+	 * @var string
+	 */
+	protected $strTable = 'tl_iso_orders';
+
+	/**
+	 * Name of the child table
+	 * @var string
+	 */
+	protected $ctable = 'tl_iso_order_items';
+
+	/**
+	 * This current order's unique ID with eventual prefix
+	 * @param string
+	 */
+	protected $strOrderId = '';
+
+	/**
+	 * Template
+	 * @var string
+	 */
+  protected $strTemplate = 'iso_delivery_note';
+
+	/**
+	 * Return a value
+	 * @param string
+	 * @return mixed
+	 */
+	public function __get($strKey)
+	{
+		switch ($strKey)
+		{
+			case 'order_id':
+				return $this->strOrderId;
+
+			case 'billingAddress':
+				return deserialize($this->arrData['billing_address'], true);
+
+			case 'shippingAddress':
+				return deserialize($this->arrData['shipping_address'], true);
+			
+			case 'paid':
+				return (((int) $this->date_paid) >= time() && $this->status == 'complete');
+
+			default:
+				return parent::__get($strKey);
+		}
+	}
+
+
+
+	/**
+	 * Find a record by its reference field and return true if it has been found
+	 * @param string
+	 * @param mixed
+	 * @return boolean
+	 */
+	public function findBy($strRefField, $varRefId)
+	{
+		if (parent::findBy($strRefField, $varRefId))
+		{
+			$this->Shipping = null;
+			$this->Payment = null;
+			$this->strTitle = $GLOBALS['TL_LANG']['MSC']['iso_delivery_note_title'];
+
+			$objPayment = $this->Database->execute("SELECT * FROM tl_iso_payment_modules WHERE id=" . $this->payment_id);
+
+			if ($objPayment->numRows)
+			{
+				$strClass = $GLOBALS['ISO_PAY'][$objPayment->type];
+
+				try
+				{
+					$this->Payment = new $strClass($objPayment->row());
+				}
+				catch (Exception $e) {}
+			}
+
+			if ($this->shipping_id > 0)
+			{
+				$objShipping = $this->Database->execute("SELECT * FROM tl_iso_shipping_modules WHERE id=" . $this->shipping_id);
+
+				if ($objShipping->numRows)
+				{
+					$strClass = $GLOBALS['ISO_SHIP'][$objShipping->type];
+
+					try
+					{
+						$this->Shipping = new $strClass($objShipping->row());
+					}
+					catch (Exception $e) {}
+				}
+			}
+
+			// The order_id must not be stored in arrData, or it would overwrite the database on save().
+			$this->strOrderId = $this->arrData['order_id'];
+			unset($this->arrData['order_id']);
+
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Return current surcharges as array
+	 * @return array
+	 */
+	public function getSurcharges()
+	{
+		$arrSurcharges = deserialize($this->arrData['surcharges']);
+		return is_array($arrSurcharges) ? $arrSurcharges : array();
+	}
+
+}
+
---
+++ system/modules/isotope_delivery_note/dca/tl_iso_orders.php
@@ -0,0 +1,165 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2011 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Kirsten Roschanski (C) 2012 
+ * @author     Kirsten Roschanski 
+ * @package    Isotope 
+ * @license    LGPL 
+ * @filesource
+ */
+
+/**
+ * Table tl_iso_orders
+ */
+
+$GLOBALS['TL_DCA']['tl_iso_orders']['list']['operations']['print_delivery_note'] = array
+(
+		'label'               => &$GLOBALS['TL_LANG']['tl_iso_orders']['print_delivery_note'],
+		'href'                => 'key=print_delivery_note',
+		'icon'                => 'system/modules/isotope/html/document-pdf-text.png'
+);
+
+$GLOBALS['TL_DCA']['tl_iso_orders']['list']['global_operations']['print_delivery_notes'] = array
+(
+		'label'               => &$GLOBALS['TL_LANG']['tl_iso_orders']['print_delivery_notes'],
+		'href'                => 'key=print_delivery_notes',
+		'class'               => 'header_print_invoices isotope-tools',
+		'attributes'          => 'onclick="Backend.getScrollOffset();"'
+);
+
+/**
+ * Class tl_iso_orders
+ * Provide miscellaneous methods that are used by the data configuration array.
+ */
+class DeliveryNoteBackend extends Backend
+{
+
+	/**
+	 * Provide a select menu to choose orders by status and print PDF
+	 * @return string
+	 */
+	public function printDeliveryNotes()
+	{
+		$strMessage = '';
+
+		$strReturn = '
+<div id="tl_buttons">
+<a href="'.ampersand(str_replace('&key=print_delivery_notes', '', $this->Environment->request)).'" class="header_back" title="'.specialchars($GLOBALS['TL_LANG']['MSC']['backBT']).'">'.$GLOBALS['TL_LANG']['MSC']['backBT'].'</a>
+</div>
+
+<h2 class="sub_headline">'.$GLOBALS['TL_LANG']['tl_iso_orders']['print_delivery_notes'][0].'</h2>
+<form action="'.$this->Environment->request.'"  id="tl_print_delivery_notes" class="tl_form" method="post">
+<input type="hidden" name="FORM_SUBMIT" value="tl_print_delivery_notes">
+<input type="hidden" name="REQUEST_TOKEN" value="'.REQUEST_TOKEN.'">
+<div class="tl_formbody_edit">
+<div class="tl_tbox block">';
+
+		$objWidget = new SelectMenu($this->prepareForWidget($GLOBALS['TL_DCA']['tl_iso_orders']['fields']['status'], 'status'));
+
+		if ($this->Input->post('FORM_SUBMIT') == 'tl_print_delivery_notes')
+		{
+			$objOrders = $this->Database->prepare("SELECT id FROM tl_iso_orders WHERE status=?")->execute($this->Input->post('status'));
+
+			if ($objOrders->numRows)
+			{
+				$this->generateDeliveryNote($objOrders->fetchEach('id'));
+			}
+			else
+			{
+				$strMessage = '<p class="tl_gerror">'.$GLOBALS['TL_LANG']['MSC']['noOrders'].'</p>';
+			}
+		}
+
+		return $strReturn . $strMessage . $objWidget->parse() . '
+</div>
+</div>
+<div class="tl_formbody_submit">
+<div class="tl_submit_container">
+<input type="submit" name="print_delivery_notes" id="ctrl_print_delivery_notes" value="'.$GLOBALS['TL_LANG']['MSC']['labelSubmit'].'">
+</div>
+</div>
+</form>
+</div>';
+	}
+
+
+	/**
+	 * Print one order as PDF
+	 * @param DataContainer
+	 * @return void
+	 */
+	public function printDeliveryNote(DataContainer $dc)
+	{
+		$this->generateDeliveryNote(array($dc->id));
+	}
+
+
+	/**
+	 * Generate one or multiple PDFs by order ID
+	 * @param array
+	 * @return void
+	 */
+	public function generateDeliveryNote(array $arrIds)
+	{
+		$this->import('Isotope');
+
+		if (!count($arrIds))
+		{
+			$this->log('No order IDs passed to method.', __METHOD__, TL_ERROR);
+			$this->redirect($this->Environment->script . '?act=error');
+		}
+
+		$pdf = null;
+
+		foreach ($arrIds as $intId)
+		{
+			$objOrder = new IsotopeDeliveryNote();
+
+			if ($objOrder->findBy('id', $intId))
+			{
+				$pdf = $objOrder->generatePDF(null, $pdf, false);
+			}
+		}
+
+		if (!$pdf)
+		{
+			$this->log('No order IDs passed to method.', __METHOD__, TL_ERROR);
+			$this->redirect($this->Environment->script . '?act=error');
+		}
+
+		// Close and output PDF document
+		$pdf->lastPage();
+
+		// @todo make things like this configurable in a further version of Isotope
+		$strDeliveryNoteTitle = 'delivery_note_' . $objOrder->order_id;
+		$pdf->Output(standardize(ampersand($strDeliveryNoteTitle, false), true) . '.pdf', 'D');
+
+		// Set config back to default
+		// @todo do we need that? The PHP session is ended anyway...
+		$this->Isotope->resetConfig(true);
+
+		// Stop script execution
+		exit;
+	}
+}
+	
